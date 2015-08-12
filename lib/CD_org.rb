61b83c6b46b0d37b5b9bf2c3bb957ba627bc368e
Jing Wait & Jason Lane <student@epicodus.com>
2015-08-12 15:36:40 -0700

    Add an add method

--- lib/CD_org.rb
+++ lib/CD_org.rb
@@ -1,9 +1,15 @@
 class CD_ORG
 
   attr_reader :cds
+
   def initialize
     @cds = []
   end
+
+  def add(cd_name)
+    @cds.push(cd_name)
+  end
+  
 
 
 end
--- spec/CD_org_spec.rb
+++ spec/CD_org_spec.rb
@@ -2,10 +2,19 @@ require 'rspec'
 require './lib/CD_org'
 
 describe CD_ORG do
+  before do
+    @org = CD_ORG.new
+  end
   describe "#initialize" do
-    it 'creates an instance of CD_org' do
-      @org = CD_ORG.new
+    it 'creates an instance of CD_ORG' do
       expect(@org.cds).to eq []
+    end
+  end
+  describe "#add" do
+    it 'adds a cd to CD_ORG' do
+      @org.add("Dookie")
+      #includes_dookie = @org.cds.include?('Dookie')
+      expect(@org.cds).to eq ["Dookie"]
     end
   end
 end
